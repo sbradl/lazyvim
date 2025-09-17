8494ef4e86bbb123b14c45ac2175acdae61dd3c5
Stefan Bradl <bradlstefan@googlemail.com>
2025-09-17 20:05:45 +0200

    refactoring: csharp config split up

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,13 +1,5 @@
 return {
   {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "csharp-language-server",
-      },
-    },
-  },
-  {
     "nvim-neotest/neotest",
     dependencies = {
       "nsidorenco/neotest-vstest",
