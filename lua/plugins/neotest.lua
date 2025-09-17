f7d6b80350b68a941896078744b09aedf63e240b
Stefan Bradl <bradlstefan@googlemail.com>
2025-09-17 20:17:42 +0200

    added neotest adapter for vitest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -3,10 +3,12 @@ return {
     "nvim-neotest/neotest",
     dependencies = {
       "nsidorenco/neotest-vstest",
+      "marilari88/neotest-vitest",
     },
     opts = {
       adapters = {
         "neotest-vstest",
+        "neotest-vitest",
       },
     },
   },
