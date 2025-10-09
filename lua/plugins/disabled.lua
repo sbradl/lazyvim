ae6bd5d2420b302007d06f6fb59449c63569b46c
Stefan Bradl <bradlstefan@googlemail.com>
2025-10-09 19:40:37 +0200

    disable noice messages

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,12 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = {
+        enabled = true,
+      },
+      messages = { enabled = false },
+    },
+  },
 }
