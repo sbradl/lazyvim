373094341934b9edc45e72da03dd5d476cdc1d02
Stefan Bradl <bradlstefan@googlemail.com>
2026-02-12 20:11:58 +0100

    Dotnet new integration for snacks explorer (A)

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,6 +5,34 @@ return {
       dashboard = { enabled = false },
       explorer = { enabled = false },
       scroll = { enabled = false },
+      picker = {
+        sources = {
+          explorer = {
+            win = {
+              list = {
+                keys = {
+                  ["A"] = "explorer_add_dotnet",
+                },
+              },
+            },
+            actions = {
+              explorer_add_dotnet = function(picker)
+                local dir = picker:dir()
+                local easydotnet = require("easy-dotnet")
+
+                easydotnet.create_new_item(dir, function(item_path)
+                  local tree = require("snacks.explorer.tree")
+                  local actions = require("snacks.explorer.actions")
+                  tree:open(dir)
+                  tree:refresh(dir)
+                  actions.update(picker, { target = item_path })
+                  picker:focus()
+                end)
+              end,
+            },
+          },
+        },
+      },
     },
   },
 }
