47fbdfb4afa74c13887f4d70d2afbf64b6a2fa39
Stefan Bradl <bradlstefan@googlemail.com>
2026-02-13 16:18:54 +0100

    vstest removed

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -3,17 +3,11 @@ return {
     "nvim-neotest/neotest",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
-      "nsidorenco/neotest-vstest",
       -- "marilari88/neotest-vitest",
       "nvim-neotest/neotest-jest",
     },
     opts = {
       adapters = {
-        ["neotest-vstest"] = {
-          dap_settings = {
-            type = "coreclr",
-          },
-        },
         -- "neotest-vitest",
         "neotest-jest",
       },
