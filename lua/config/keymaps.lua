e73c1cdb4261cd7a20e5b91d2c122a09434e8d2b
Stefan Bradl <bradlstefan@googlemail.com>
2026-04-10 19:28:24 +0200

    C-Arrow for moving to window and S-Arrow for resizing

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("n", "<S-Tab>", "<cmd>e #<cr>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-Left>", "<C-w>h", { desc = "Go to Left window", remap = true })
+vim.keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Go to Lower window", remap = true })
+vim.keymap.set("n", "<C-Up>", "<C-w>k", { desc = "Go to Upper window", remap = true })
+vim.keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Go to Right window", remap = true })
+
+vim.keymap.set("n", "<S-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.keymap.set("n", "<S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+vim.keymap.set("n", "<S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Increase Window Width" })
+vim.keymap.set("n", "<S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Decrease Window Width" })
