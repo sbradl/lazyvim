2797824c0977deaf90cf56fd59ce05b109af21e8
Stefan Bradl <bradlstefan@googlemail.com>
2026-07-07 16:51:34 +0200

    pwsh under win32 as shell

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,3 +11,7 @@ vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
+
+if vim.fn.has("win32") == 1 and vim.fn.executable("pwsh.exe") == 1 then
+  vim.o.shell = "pwsh.exe"
+end
