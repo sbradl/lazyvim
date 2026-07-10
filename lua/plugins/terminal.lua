76b6406fb3b88ade7a27d2e5bf7c6e2ff9890136
Stefan Bradl <bradlstefan@googlemail.com>
2026-07-10 10:51:54 +0200

    updates

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -8,8 +8,16 @@ return {
       vim.keymap.set("n", "gt", t.focus_last_terminal, { desc = "Go to Terminal" })
       vim.keymap.set("n", "gT", t.open_new_terminal, { desc = "Open New Terminal", remap = true })
       vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+    end,
+  },
+  {
+    "sbradl/command_runner.nvim",
+    dependencies = { "sbradl/terminal.nvim" },
+    config = function()
+      local r = require("command_runner")
 
-      vim.keymap.set("n", "<leader>rr", t.run_command, { desc = "Run command for current file", remap = true })
+      vim.keymap.set("n", "<leader>rc", r.run_command, { desc = "Run command for current file", remap = true })
+      -- vim.keymap.set("n", "<leader>rr", r.run_last_command, { desc = "Rerun previous command", remap = true })
     end,
   },
 }
