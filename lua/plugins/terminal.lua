9ec1ea7b7d3e070309dc2cb9bca4fe7b8bdece1a
Stefan Bradl <bradlstefan@googlemail.com>
2026-07-13 20:11:30 +0200

    open new terminal with Ctrl+t

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -6,7 +6,7 @@ return {
       t.setup()
 
       vim.keymap.set("n", "gt", t.focus_last_terminal, { desc = "Go to Terminal" })
-      vim.keymap.set("n", "gT", t.open_new_terminal, { desc = "Open New Terminal", remap = true })
+      vim.keymap.set("n", "<C-t>", t.open_new_terminal, { desc = "Open New Terminal", remap = true })
       vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
     end,
   },
