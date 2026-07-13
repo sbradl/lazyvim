1fd18d18ac6e1b68751745b58664cc7dcce445ea
Stefan Bradl <bradlstefan@googlemail.com>
2026-07-13 20:23:46 +0200

    removed telescope which was not used

--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -3,7 +3,6 @@ return {
     "GustavEikaas/easy-dotnet.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
     },
     config = function()
       require("easy-dotnet").setup({
