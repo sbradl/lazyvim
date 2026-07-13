5dfac043b8b5ec255304d1694e281e68d8d4b15d
Stefan Bradl <bradlstefan@googlemail.com>
2026-07-13 20:11:19 +0200

    rounded borders for better visibility

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,6 +12,8 @@ vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
+vim.o.pumborder = "rounded"
+vim.o.winborder = "rounded"
 
 if vim.fn.has("win32") == 1 and vim.fn.executable("pwsh.exe") == 1 then
   vim.o.shell = "pwsh.exe"
