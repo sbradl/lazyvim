2796b58788cd2b6049ec2b5e7df1e7dd80981582
Stefan Bradl <bradlstefan@googlemail.com>
2026-08-10 10:19:01 +0200

    debug: swapped keymaps for step out and step over

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,6 +3,8 @@ return {
     "mfussenegger/nvim-dap",
     keys = {
       { "<leader>dr", false },
+      { "<leader>do", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<leader>dO", function() require("dap").step_out() end, desc = "Step Out" },
     },
   },
 }
