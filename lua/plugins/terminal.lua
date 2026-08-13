c945a8666acc044dd09ec1bec8058e8a80b2cf03
Stefan Bradl <bradlstefan@googlemail.com>
2026-08-13 19:39:08 +0200

    <leader>t to open new terminal in project root directory

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -7,6 +7,9 @@ return {
 
       vim.keymap.set("n", "gt", t.focus_last_terminal, { desc = "Go to Terminal" })
       vim.keymap.set("n", "<C-t>", t.open_new_terminal, { desc = "Open New Terminal", remap = true })
+      vim.keymap.set("n", "<leader>t", function()
+        t.open_new_terminal(LazyVim.root())
+      end, { desc = "Open Terminal in Project Root", remap = true })
       vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
     end,
   },
@@ -19,7 +22,7 @@ return {
 
       vim.keymap.set("n", "<leader>rc", r.run_command, { desc = "Run command for current file", remap = true })
       vim.keymap.set("n", "<leader>rh", r.show_history, { desc = "Show command history", remap = true })
-      -- vim.keymap.set("n", "<leader>rr", r.rerun_command, { desc = "Rerun previous command", remap = true })
+
       require("which-key").add({
         {
           "<leader>rr",
